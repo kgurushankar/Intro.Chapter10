916fa17652fe634a84583c7b8fee3ae96269e5de
kgurushankar <keshavgurushankar@hotmail.com>
2017-04-01 21:20:25 -0700

    Added a personal utility method

---
+++ src/menuprint/MenuGenerator.java
@@ -0,0 +1,20 @@
+package menuprint;
+
+public class MenuGenerator {
+
+	private MenuGenerator() {
+		// should not be able to be constructed
+	}
+
+	public static String createMenu(String[] options) {
+		StringBuilder out = new StringBuilder();
+		for (int i = 0; i < options.length; i++) {
+			out.append(i + 1);
+			out.append(" - ");
+			out.append(options[i]);
+			out.append("\n");
+		}
+		return out.toString();
+	}
+
+}
